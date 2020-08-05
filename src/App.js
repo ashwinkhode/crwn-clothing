4d8a1eb2a2e062cfe325cdecb9877fc51e1fac27
Ashwin Khode <ashwin.khode1@gmail.com>
2020-08-05 23:47:20 +0530

    Forgot to commit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+            <Header />
+              <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop' component={ShopPage} />
+                <Route path='/signin' component={SignInAndSignUpPage} />
+              </Switch>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import SHOP_DATA from './shop.data'
+
+import '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA   
+        };
+
+    }
+    render() {
+        const {collections} = this.state;
+        return (
+        <div className='shop-page'>
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+        </div>
+        )
+    }
+}
+
+export default ShopPage
